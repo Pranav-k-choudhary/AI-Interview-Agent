9fa5d698f92201742c6fbb2db6c32d246df1bc9b
Pranav-K-choudhary <pranavkumar2000gayghat@gmail.com>
2026-07-22 21:28:05 +0530

    Update frontend API for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  withCredentials: true, // Crucial for cookie-based authentication
+  baseURL:
+    import.meta.env.MODE === 'development'
+      ? 'http://localhost:5000/api'
+      : 'https://ai-interview-agent-backend-ri2p.onrender.com/api',
+
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
